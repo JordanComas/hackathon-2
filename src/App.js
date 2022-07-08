6963430c22c3f9067dad0413729ae7f5a7bfaeb3
Miguel Rocha <mrocha1095@gmail.com>
2022-07-08 15:16:34 -0400

    car list

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import Contact from "./components/Contact";
 import Login from "./components/Login";
 import NotFound from "./components/NotFound";
 import About from "./components/About";
+import Carlist from "./components/CarList";
 
 function App() {
+
+  <Link to='/car-list'>Search Cars</Link>
+
   return (
     <div className="App">
       <Routes>
@@ -15,6 +19,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/car-list" element={<Carlist />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
